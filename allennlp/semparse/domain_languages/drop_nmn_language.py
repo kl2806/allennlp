3afa9349404e3953eee2762b6cbde2b4e5ceaae7
Kevin Lin <kevinl@kevinl.local>
2019-01-30 14:20:59 -0800

    Add different answer types

--- allennlp/semparse/domain_languages/drop_nmn_language.py
+++ allennlp/semparse/domain_languages/drop_nmn_language.py
@@ -2,23 +2,33 @@ from typing import Dict, List, Tuple
 
 import torch
 from torch import Tensor
-from torch.nn import Parameter
+from torch.nn import Parameter, LSTM
 
 from allennlp.semparse.domain_languages.domain_language import (DomainLanguage, predicate,
                                                                 predicate_with_side_args)
 
-
 # An Attention is a single tensor; we're giving this a type so that we can use it for constructing
 # predicates.
-class Attention(Tensor):
+class AttentionTensor(Tensor):
     pass
 
 
-# An Answer is a distribution over answer options, which is a tensor.  Again, we're subclassing
-# Tensor here to give a concrete type for our predicates to use.
-class Answer(Tensor):
+# A ``SpanAnswer`` is a distribution over spans in the passage. It has a distribution over the start
+# span and the end span.
+class SpanAnswer:
+    def __init__(self,
+                 start_index: Tensor,
+                 end_index: Tensor) -> None:
+        self.start_index = start_index
+        self.end_index = end_index
+
+# A ``NumberAnswer`` is a distribution over the possible answers from addition and subtraction.
+class NumberAnswer(Tensor):
     pass
 
+# A ``CountAnswer`` is a distribution over the possible counts in the passage.
+class CountAnswer(Tensor):
+    pass
 
 class DropNmnParameters(torch.nn.Module):
     """
@@ -33,47 +43,38 @@ class DropNmnParameters(torch.nn.Module):
                  question_encoding_dim: int,
                  passage_encoding_dim: int,
                  hidden_dim: int,
-                 num_answers: int) -> None:
+                 num_number_answers: int,
+                 num_count_answers: int,
+                 num_span_answers: int) -> None:
         super().__init__()
-        # The paper calls these "convolutions", but if you look at the code, it just does a 1x1
-        # convolution, which is a linear transformation on the last dimension.  I'm still calling
-        # them "convs" here, to match the original paper, and because they are operating on
-        # image-feature-shaped things, and we might change our mind later about how to parameterize
-        # them.  All of these parameters try to match what you see in table 1 of the paper, where
-        # we write "W" as "linear".
-        self.find_conv1 = torch.nn.Linear(passage_encoding_dim, question_encoding_dim)
-        self.find_conv2 = torch.nn.Linear(question_encoding_dim, 1)
-        self.find_linear = torch.nn.Linear(question_encoding_dim, question_encoding_dim) 
+        from allennlp.modules.attention.dot_product_attention import DotProductAttention
+        self.find_attention = DotProductAttention()
+
         self.relocate_conv1 = torch.nn.Linear(passage_encoding_dim, hidden_dim)
         self.relocate_conv2 = torch.nn.Linear(hidden_dim, 1)
         self.relocate_linear1 = torch.nn.Linear(passage_encoding_dim, 1)
         self.relocate_linear2 = torch.nn.Linear(question_encoding_dim, hidden_dim)
 
-        self.exist_linear = torch.nn.Linear(passage_length, num_answers)
-        self.count_linear = torch.nn.Linear(passage_length, num_answers)
+        self.count_lstm = LSTM(1,2)
+        self.count_linear = torch.nn.Linear(2, num_count_answers)
 
-        self.describe_linear1 = torch.nn.Linear(hidden_dim, num_answers)
-        self.describe_linear2 = torch.nn.Linear(passage_encoding_dim, hidden_dim)
-        self.describe_linear3 = torch.nn.Linear(question_encoding_dim, hidden_dim)
+        self.more_linear1 = torch.nn.Linear(passage_length, num_span_answers)
+        self.more_linear2 = torch.nn.Linear(passage_length, num_span_answers)
 
-        self.count_equals_linear1 = torch.nn.Linear(passage_length, num_answers)
-        self.count_equals_linear2 = torch.nn.Linear(passage_length, num_answers)
+        self.less_linear1 = torch.nn.Linear(passage_length, num_span_answers)
+        self.less_linear2 = torch.nn.Linear(passage_length, num_span_answers)
 
-        self.more_linear1 = torch.nn.Linear(passage_length, num_answers)
-        self.more_linear2 = torch.nn.Linear(passage_length, num_answers)
-
-        self.less_linear1 = torch.nn.Linear(passage_length, num_answers)
-        self.less_linear2 = torch.nn.Linear(passage_length, num_answers)
-
-        self.compare_linear1 = torch.nn.Linear(hidden_dim, num_answers)
+        self.compare_linear1 = torch.nn.Linear(hidden_dim, num_span_answers)
         self.compare_linear2 = torch.nn.Linear(passage_encoding_dim, hidden_dim)
         self.compare_linear3 = torch.nn.Linear(passage_encoding_dim, hidden_dim)
         self.compare_linear4 = torch.nn.Linear(question_encoding_dim, hidden_dim)
 
+        self.num_number_answers = num_number_answers
+
 
 class DropNmnLanguage(DomainLanguage):
     """
-    DomainLanguage for the DROP dataset based on neural module networks This language has a `learned execution model`,
+    DomainLanguage for the DROP dataset based on neural module networks. This language has a `learned execution model`,
     meaning that the predicates in this language have learned parameters.
 
     Parameters
@@ -83,18 +84,17 @@ class DropNmnLanguage(DomainLanguage):
     """
 
     def __init__(self, encoded_passage: Tensor, parameters: DropNmnParameters) -> None:
-        super().__init__(start_types={Answer})
-        self.encoded_passage = encoded_passage 
+        super().__init__(start_types={CountAnswer, SpanAnswer, NumberAnswer})
+        self.encoded_passage = encoded_passage
         self.parameters = parameters
 
     @predicate_with_side_args(['attended_question'])
-    def find(self, attended_question: Tensor) -> Attention:
-        conv1 = self.parameters.find_conv1
-        conv2 = self.parameters.find_conv2
-        return conv2(conv1(self.encoded_passage) * attended_question).squeeze()
+    def find(self, attended_question: Tensor) -> AttentionTensor:
+        find = self.parameters.find_attention
+        return find(attended_question.unsqueeze(0), self.encoded_passage.unsqueeze(0))
 
     @predicate_with_side_args(['attended_question'])
-    def relocate(self, attention: Attention, attended_question: Tensor) -> Attention:
+    def relocate(self, attention: AttentionTensor, attended_question: Tensor) -> AttentionTensor:
         conv1 = self.parameters.relocate_conv1
         conv2 = self.parameters.relocate_conv2
         linear1 = self.parameters.relocate_linear1
@@ -102,38 +102,25 @@ class DropNmnLanguage(DomainLanguage):
         attended_passage = (attention.unsqueeze(-1)* self.encoded_passage).sum(dim=[0])
         return conv2(conv1(self.encoded_passage) * linear1(attended_passage) * linear2(attended_question)).squeeze()
 
-    @predicate_with_side_args(['attended_question'])
-    def filter(self, attention: Attention, attended_question: Tensor) -> Attention:
-        return self.and_(attention, self.find(attended_question))
-
     @predicate
-    def and_(self, attention1: Attention, attention2: Attention) -> Attention:
+    def and_(self, attention1: AttentionTensor, attention2: AttentionTensor) -> AttentionTensor:
         return torch.max(torch.stack([attention1, attention2], dim=0), dim=0)[0]
 
     @predicate
-    def or_(self, attention1: Attention, attention2: Attention) -> Attention:
+    def or_(self, attention1: AttentionTensor, attention2: AttentionTensor) -> AttentionTensor:
         return torch.min(torch.stack([attention1, attention2], dim=0), dim=0)[0]
 
     @predicate
-    def exist(self, attention: Attention) -> Answer:
-        linear = self.parameters.exist_linear
-        return linear(attention.view(-1))
-
-    @predicate
-    def count(self, attention: Attention) -> Answer:
+    def count(self, attention: AttentionTensor) -> CountAnswer:
+        lstm = self.parameters.count_lstm
         linear = self.parameters.count_linear
-        return linear(attention.view(-1))
+
+        # (1, passage_length, 2)
+        hidden_states  = lstm(attention.unsqueeze(-1))[0]
+        return linear(hidden_states.squeeze()[-1])
 
     @predicate_with_side_args(['attended_question'])
-    def describe(self, attention: Attention, attended_question: Tensor) -> Answer:
-        linear1 = self.parameters.describe_linear1
-        linear2 = self.parameters.describe_linear2
-        linear3 = self.parameters.describe_linear3
-        attended_passage = (attention.unsqueeze(-1) * self.encoded_passage).sum(dim=[0])
-        return linear1(linear2(attended_passage) * linear3(attended_question))
-
-    @predicate_with_side_args(['attended_question'])
-    def compare(self, attention1: Attention, attention2: Attention, attended_question: Tensor) -> Answer:
+    def compare(self, attention1: AttentionTensor, attention2: AttentionTensor, attended_question: Tensor) -> SpanAnswer:
         linear1 = self.parameters.compare_linear1
         linear2 = self.parameters.compare_linear2
         linear3 = self.parameters.compare_linear3
@@ -142,45 +129,12 @@ class DropNmnLanguage(DomainLanguage):
         attended_passage2 = (attention2.unsqueeze(-1) * self.encoded_passage).sum(dim=[0])
         return linear1(linear2(attended_passage1) * linear3(attended_passage2) * linear4(attended_question))
 
-    @predicate_with_side_args(['attention_map'])
-    def add_(self,
-             attention1: Attention, 
-             attention2: Attention,
-             attention_map: Dict[int, List[Tuple[int, int]]]) -> Answer:
-
-        attention_product = torch.matmul(attention1.unsqueeze(-1), torch.t(attention2.unsqueeze(-1)))
-        answers = torch.zeros(len(attention_map),)
-
-        for candidate_index, (candidate_addition, indices) in enumerate(attention_map.items()):
-            attention_sum = 0
-            for index1, index2 in indices:
-                attention_sum += attention_product[index1, index2]
-            answers[candidate_index] = attention_sum
-        return answers
-
-    @predicate
-    def count_equals(self, attention1: Attention, attention2: Attention) -> Answer:
-        linear1 = self.parameters.count_equals_linear1
-        linear2 = self.parameters.count_equals_linear2
-        return linear1(attention1.view(-1)) + linear2(attention2.view(-1))
-
-    @predicate
-    def more(self, attention1: Attention, attention2: Attention) -> Answer:
-        linear1 = self.parameters.more_linear1
-        linear2 = self.parameters.more_linear2
-        return linear1(attention1.view(-1)) + linear2(attention2.view(-1))
-
-    @predicate
-    def less(self, attention1: Attention, attention2: Attention) -> Answer:
-        linear1 = self.parameters.less_linear1
-        linear2 = self.parameters.less_linear2
-        return linear1(attention1.view(-1)) + linear2(attention2.view(-1))
-
+    
     @predicate_with_side_args(['attention_map'])
     def subtract_(self,
-                  attention1: Attention,
-                  attention2: Attention,
-                  attention_map: Dict[int, List[Tuple[int, int]]]) -> Answer:
+                  attention1: AttentionTensor,
+                  attention2: AttentionTensor,
+                  attention_map: Dict[int, List[Tuple[int, int]]]) -> NumberAnswer:
         attention_product = torch.matmul(attention1.unsqueeze(-1), torch.t(attention2.unsqueeze(-1)))
         answers = torch.zeros(len(attention_map),)
         for candidate_index, (candidate_subtraction, indices) in enumerate(attention_map.items()):
@@ -188,5 +142,19 @@ class DropNmnLanguage(DomainLanguage):
             for index1, index2 in indices:
                 attention_sum += attention_product[index1, index2]
             answers[candidate_index] = attention_sum
+        return NumberAnswer(answers)
 
-        return answers
+    @predicate_with_side_args(['attention_map'])
+    def add_(self,
+             attention1: AttentionTensor,
+             attention2: AttentionTensor,
+             attention_map: Dict[int, List[Tuple[int, int]]]) -> NumberAnswer:
+        attention_product = torch.matmul(attention1.unsqueeze(-1), torch.t(attention2.unsqueeze(-1)))
+        answers = torch.zeros(len(attention_map),)
+        for candidate_index, (candidate_addition, indices) in enumerate(attention_map.items()):
+            attention_sum = 0
+            for index1, index2 in indices:
+                attention_sum += attention_product[index1, index2]
+            answers[candidate_index] = attention_sum
+        return NumberAnswer(answers)
+
--- allennlp/tests/semparse/domain_languages/drop_nmn_language_test.py
+++ allennlp/tests/semparse/domain_languages/drop_nmn_language_test.py
@@ -15,14 +15,20 @@ class DropNmnLanguageTest(AllenNlpTestCase):
         self.num_answers = 7
 
         self.passage_length = 11
-        self.passage_encoding_dim = 8
+        self.passage_encoding_dim = 6
+        
+        self.num_count_answers= 10 
+        self.num_number_answers= 11
+        self.num_span_answers= 12
 
         self.encoded_passage = torch.rand(self.passage_length, self.passage_encoding_dim)
         self.parameters = DropNmnParameters(passage_length=self.passage_length,
                                             question_encoding_dim=self.text_encoding_dim,
                                             passage_encoding_dim=self.passage_encoding_dim,
                                             hidden_dim=self.hidden_dim,
-                                            num_answers=self.num_answers)
+                                            num_count_answers=self.num_count_answers,
+                                            num_number_answers=self.num_number_answers,
+                                            num_span_answers=self.num_span_answers)
 
         self.language = DropNmnLanguage(self.encoded_passage, self.parameters)
 
@@ -78,46 +84,12 @@ class DropNmnLanguageTest(AllenNlpTestCase):
         new_attention = self.language.or_(attention1, attention2)
         assert new_attention.size() == (self.passage_length,)
 
-    def test_exist_returns_correct_shape(self):
-        attention = torch.rand(self.passage_length,)
-        answer = self.language.exist(attention)
-        assert answer.size() == (self.num_answers,)
 
     def test_count_returns_correct_shape(self):
-        attention = torch.rand(self.passage_length,)
+        attention = torch.rand(1, self.passage_length)
         answer = self.language.count(attention)
-        assert answer.size() == (self.num_answers,)
+        assert answer.size() == (self.num_count_answers,)
 
-    def test_describe_returns_correct_shape(self):
-        attended_question = torch.rand(self.text_encoding_dim)
-        attention = torch.rand(self.passage_length,)
-        answer = self.language.describe(attention, attended_question)
-        assert answer.size() == (self.num_answers,)
-
-    def test_compare_returns_correct_shape(self):
-        attended_question = torch.rand(self.text_encoding_dim)
-        attention1 = torch.rand(self.passage_length,)
-        attention2 = torch.rand(self.passage_length,)
-        answer = self.language.compare(attention1, attention2, attended_question)
-        assert answer.size() == (self.num_answers,)
-
-    def test_count_equals_returns_correct_shape(self):
-        attention1 = torch.rand(self.passage_length,)
-        attention2 = torch.rand(self.passage_length,)
-        answer = self.language.count_equals(attention1, attention2)
-        assert answer.size() == (self.num_answers,)
-
-    def test_more_returns_correct_shape(self):
-        attention1 = torch.rand(self.passage_length,)
-        attention2 = torch.rand(self.passage_length,)
-        answer = self.language.more(attention1, attention2)
-        assert answer.size() == (self.num_answers,)
-
-    def test_less_returns_correct_shape(self):
-        attention1 = torch.rand(self.passage_length,)
-        attention2 = torch.rand(self.passage_length,)
-        answer = self.language.less(attention1, attention2)
-        assert answer.size() == (self.num_answers,)
 
     def test_addition_indices(self):
         attention1 = torch.tensor([0, 1, 1])
@@ -145,26 +117,24 @@ class DropNmnLanguageTest(AllenNlpTestCase):
         # computation performed by each function, because there are learned parameters in there.
         # We'll treat this as similar to a model test, just making sure the tensor operations work.
         attended_question = {'attended_question': torch.rand(self.text_encoding_dim)}
-        # A simple one to start with: (exist find)
-        action_sequence = ['@start@ -> Answer',
-                           'Answer -> [<Attention:Answer>, Attention]',
-                           '<Attention:Answer> -> exist',
-                           'Attention -> find']
+        # A simple one to start with: (count find)
+        action_sequence = ['@start@ -> CountAnswer', 'CountAnswer -> [<AttentionTensor:CountAnswer>, AttentionTensor]',
+                           '<AttentionTensor:CountAnswer> -> count', 'AttentionTensor -> find']
+
         assert self.language.execute_action_sequence(action_sequence, [attended_question] * len(action_sequence)).shape == (self.num_answers,)
 
     def test_nmn_action_sequence(self):
-        logical_form = "(exist find)"
+        # A simple one to start with: find two numbers and add them
+        logical_form = "(add_ find find)"
         action_sequence = self.language.logical_form_to_action_sequence(logical_form)
+        assert action_sequence == ['@start@ -> NumberAnswer', 'NumberAnswer -> [<AttentionTensor,AttentionTensor:NumberAnswer>, AttentionTensor, AttentionTensor]',
+                                   '<AttentionTensor,AttentionTensor:NumberAnswer> -> add_', 'AttentionTensor -> find', 'AttentionTensor -> find']
 
-        logical_form = "(count_equals find find)"
+        # ``How many field goals did Nick Folk attempt?``
+        logical_form = "(count find)"
         action_sequence = self.language.logical_form_to_action_sequence(logical_form)
-
-        logical_form = "(add_numbers find find)"
-        action_sequence = self.language.logical_form_to_action_sequence(logical_form)
-
-        logical_form = "(describe (and_ find find))"
-        action_sequence = self.language.logical_form_to_action_sequence(logical_form)
-        print(action_sequence)
+        assert action_sequence == ['@start@ -> CountAnswer', 'CountAnswer -> [<AttentionTensor:CountAnswer>, AttentionTensor]',
+                                   '<AttentionTensor:CountAnswer> -> count', 'AttentionTensor -> find']
 
 
 
