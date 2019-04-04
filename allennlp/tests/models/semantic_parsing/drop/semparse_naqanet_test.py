89ef0782c7c5d9524891908131a71294adcbb750
Kevin Lin <kevinl@kevinl.local>
2019-04-04 10:38:22 -0700

    Add at est

---
+++ allennlp/tests/models/semantic_parsing/drop/semparse_naqanet_test.py
@@ -0,0 +1,16 @@
+#pylint: disable=unused-import
+from flaky import flaky
+
+from allennlp.common.testing import ModelTestCase
+
+
+class SemparseNumericallyAugmentedQaNetTest(ModelTestCase):
+    def setUp(self):
+        super().setUp()
+        print(self.FIXTURES_ROOT)
+        self.set_up_model(self.FIXTURES_ROOT / "semparse_naqanet" / "experiment.json",
+                          self.FIXTURES_ROOT / "data" / "drop.json")
+
+    @flaky
+    def test_semparse_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
